6c7dd441cf02ea39021c03bf91df01d64bb51e0e
Dang <lequocdangbku18@gmail.com>
2022-07-30 11:42:36 +0700

    remote

---
+++ app/src/main/java/com/example/appsale/data/remote/ApiService.java
@@ -0,0 +1,32 @@
+package com.example.appsale.data.remote;
+
+import com.example.appsale29032022.data.remote.dto.AppResource;
+import com.example.appsale29032022.data.remote.dto.FoodDTO;
+import com.example.appsale29032022.data.remote.dto.OrderDTO;
+import com.example.appsale29032022.data.remote.dto.UserDTO;
+
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+/**
+ * Created by pphat on 7/14/2022.
+ */
+public interface ApiService {
+
+    @POST("user/sign-in")
+    Call<AppResource<UserDTO>> signIn(@Body HashMap<String, Object> body);
+
+    @POST("user/sign-up")
+    Call<AppResource<UserDTO>> signUp(@Body HashMap<String, Object> body);
+
+    @GET("product")
+    Call<AppResource<List<FoodDTO>>> fetchFoods();
+
+    @POST("cart/add")
+    Call<AppResource<OrderDTO>> addToCart(@Body HashMap<String, String> body);
+}
